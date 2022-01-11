1267119baae795847858fa2eb8c2ad307de215d4
sun <sun@codebrick.co>
2022-01-11 18:43:05 +0900

    StudentTeacher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from "@nestjs/common";
 import { StudentController } from "../student/student.controller";
+import { TeacherController } from "../teacher/teacher.controller";
+import { StudentTeacherController } from "../teacher/student.controller";
 
 //root of the application
 @Module({
     imports: [],
-    controllers: [StudentController],
+    controllers: [
+        StudentController,
+        TeacherController,
+        StudentTeacherController,
+    ],
 })
 export class AppModule {}
