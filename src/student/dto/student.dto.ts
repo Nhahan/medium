603ebe79d9c9099ebd5638aeaa1a4046628eb563
sun <sun@codebrick.co>
2022-01-12 11:00:15 +0900

    specitic dtos

--- src/student/dto/student.dto.ts
+++ src/student/dto/student.dto.ts
@@ -1,3 +1,14 @@
+export class FindStudentResponseDto {
+    id: string;
+    name: string;
+    teacher: string;
+}
+
+export class StudentResponseDto {
+    id: string;
+    name: string;
+    teacher: string;
+}
 export class CreateStudentDto {
     name: string;
     teacher: string;
--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,20 +1,27 @@
 import { Controller, Get, Post, Put, Param, Body } from "@nestjs/common";
-import { CreateStudentDto, UpdateStudentDto } from "./dto/student.dto";
+import {
+    CreateStudentDto,
+    FindStudentResponseDto,
+    UpdateStudentDto,
+    StudentResponseDto,
+} from "./dto/student.dto";
 
 @Controller("students") // '/students'
 export class StudentController {
     @Get()
-    getStudents() {
+    getStudents(): FindStudentResponseDto[] {
         return "All Students";
     }
 
     @Get("/:studentId")
-    getStudentById(@Param("studentId") studentId: string) {
+    getStudentById(
+        @Param("studentId") studentId: string,
+    ): FindStudentResponseDto {
         return `Get Student With Id of ${studentId}`;
     }
 
     @Post()
-    createStudent(@Body() body: CreateStudentDto) {
+    createStudent(@Body() body: CreateStudentDto): StudentResponseDto {
         return `Create Student With The Following Data ${JSON.stringify(body)}`;
     }
 
@@ -22,7 +29,7 @@ export class StudentController {
     updateStudent(
         @Param("studentId") studentId: string,
         @Body() body: UpdateStudentDto,
-    ) {
+    ): StudentResponseDto {
         return `Update Student With The Following Data ${JSON.stringify(
             body,
         )} and ${studentId}`;
