deb6901590a60a06261e1bd0644dae9aaa9ccbd7
sun <sun@codebrick.co>
2022-01-12 19:12:40 +0900

    deprecrated

--- src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { StudentModule } from "../student/student.module";
-
-//root of the application
-@Module({
-    imports: [StudentModule],
-})
-export class AppModule {}
