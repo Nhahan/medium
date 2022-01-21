ae0cdfe3789d73f0a0d2330f0c2a8f267ca3ae45
sun <sun@codebrick.co>
2022-01-21 15:10:10 +0900

    ormconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TagModule } from "./tag/tag.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import ormconfig from "./ormconfig";
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig), TagModule], // forRoot: import from not our project but from library
