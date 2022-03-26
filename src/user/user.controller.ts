aec57df675cf13a082aa6ac1fcf76e6ee28d7d47
sun <sun@codebrick.co>
2022-03-26 21:55:06 +0900

    createUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller()
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post('users')
   async createUser(): Promise<any> {
-    return 'createUser';
+    return this.userService.createUser();
   }
 }
