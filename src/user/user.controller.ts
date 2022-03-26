83d47aebff691bee2e9b93f25f000a31ac6e1841
sun <sun@codebrick.co>
2022-03-26 21:50:10 +0900

    post url

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post } from '@nestjs/common';
 
 @Controller()
 export class UserController {
-  @Post()
+  @Post('users')
   async createUser(): Promise<any> {
     return 'createUser';
   }
