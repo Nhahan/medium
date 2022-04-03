c5cc55af3fbc6889f68f99e98c4b4a9914632b97
sun <sun@codebrick.co>
2022-04-03 23:49:10 +0900

    buildUserResponse

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,20 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserEntity> {
     return this.userService.createUser(createUserDto);
+  }
+
+  generateJwt(user: UserEntity): string {}
+
+  buildUserResponse(user: UserEntity): any {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user),
+      },
+    };
   }
 }
