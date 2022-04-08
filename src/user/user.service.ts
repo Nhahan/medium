952e974f5110510bfb4a91dfe57df62352dfc624
sun <sun@codebrick.co>
2022-04-08 18:21:55 +0900

    new UserEntity

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserService {
-  async createUser(createDto: CreateUserDto) {
-    return createDto;
+  async createUser(createUserDto: CreateUserDto) {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    console.log('newUser', newUser);
   }
 }
