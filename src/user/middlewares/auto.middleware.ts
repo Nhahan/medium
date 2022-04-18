07ceb7901d7a9baceedbd246c0e0c4bb2586d607
sun <sun@codebrick.co>
2022-04-19 01:22:13 +0900

    bug fix

--- src/user/middlewares/auto.middleware.ts
+++ src/user/middlewares/auto.middleware.ts
@@ -1,7 +1,7 @@
 import { NestMiddleware } from '@nestjs/common';
 
 export class AuthMiddleware implements NestMiddleware {
-  async user(req, res, next) {
+  async use(req, res, next) {
     console.log('AuthMiddleware', req.headers);
     next();
   }
