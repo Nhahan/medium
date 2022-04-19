298c2cc0510b897b3db138d35f2d646042a1f981
sun <sun@codebrick.co>
2022-04-20 00:49:36 +0900

    exception

--- src/user/middlewares/auto.middleware.ts
+++ src/user/middlewares/auto.middleware.ts
@@ -1,8 +1,14 @@
 import { NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
 export class AuthMiddleware implements NestMiddleware {
-  async use(req, res, next) {
+  async use(req: Request, res: Response, next: NextFunction) {
     console.log('AuthMiddleware', req.headers);
+    if (!req.headers.authorization) {
+      req.user = null;
+      next;
+      return;
+    }
     next();
   }
 }
