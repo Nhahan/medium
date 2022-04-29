3b877dd5d30d271bbcd49fd244fe48b3cd4967b4
sun <sun@codebrick.co>
2022-04-29 21:39:28 +0900

    updateUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { LoginUserDto } from './dto/loginUser.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
@@ -6,6 +6,7 @@ import { UserService } from './user.service';
 import { User } from './decorators/user.decorator';
 import { UserEntity } from './user.entity';
 import { AuthGuard } from './guards/auth.guard';
+import { UpdateUserDto } from './dto/updateuser.dto';
 
 @Controller()
 export class UserController {
@@ -27,6 +28,16 @@ export class UserController {
   @Get('users')
   @UseGuards(AuthGuard)
   async currentUser(@User('id') user: UserEntity): Promise<UserResponseInterface> {
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Put('user')
+  @UseGuards(AuthGuard)
+  async updateCurrentuser(
+    @User('id') currentUserId: number,
+    @Body('user') updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.updateUser(id, updateUserDto);
     return this.userService.buildUserResponse(user);
   }
 }
