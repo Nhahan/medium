b93433448c21b776473d5400da215b986763bcc0
sun <sun@codebrick.co>
2022-05-05 18:56:02 +0900

    fix import and DI articleService

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post } from '@nestjs/common';
+import { ArticleService } from './article.service';
 
 @Controller('article')
 export class ArticleController {
+  constructor(private readonly articlesService: ArticleService) {}
   @Post()
   async create() {
     return 'Create Article';
--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,4 +1,8 @@
+import { AuthGuard } from '@app/user/guards/auth.guard';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleController } from './article.controller';
+import { ArticleService } from './article.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ArticleEntity])],
