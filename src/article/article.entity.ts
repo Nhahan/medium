feb673affc97f8686734ca0effd6488dd41ab3ec
sun <sun@codebrick.co>
2022-05-08 23:26:05 +0900

    define article entity

--- src/article/article.entity.ts
+++ src/article/article.entity.ts
@@ -1,4 +1,31 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'articles' })
-export class ArticleEntity {}
+export class ArticleEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  slug: string;
+
+  @Column()
+  title: string;
+
+  @Column({ default: '' })
+  description: string;
+
+  @Column({ default: '' })
+  body: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @Column('simple-array')
+  tagList: string[];
+
+  @Column({ default: 0 })
+  favoritesCount: number;
+}
