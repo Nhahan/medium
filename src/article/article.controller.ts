530fabe6deb6e4a7c2be99be76caa9a1c77e76ab
sun <sun@codebrick.co>
2022-05-12 00:03:06 +0900

    define body

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { User } from '@app/user/decorators/user.decorator';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { CreateArticleDto } from './dto/createArticle.dto';
 
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
   @Post()
-  async create() {
+  @UseGuards(AuthGuard)
+  async create(@User('id') currentUserId: number, @Body('article') createArticleDto: CreateArticleDto) {
     return this.articleService.createArticle();
   }
 }
