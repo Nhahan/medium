bffd0992d0bdac32f417484c46be743f05693ca2
sun <sun@codebrick.co>
2022-05-12 10:06:42 +0900

    fix entity import bug

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -2,6 +2,7 @@ import { AuthGuard } from '@app/user/guards/auth.guard';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleController } from './article.controller';
+import { ArticleEntity } from './article.entity';
 import { ArticleService } from './article.service';
 
 @Module({
